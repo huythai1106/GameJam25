9566c168a8d3e7f82ee66d68352667486b7de6ee
Đào Quang Anh <69199887+quanganhdao@users.noreply.github.com>
2025-01-18 09:17:00 +0700

    refactor mana manager

--- Assets/_FlowSystem/GameplayUI/ManaUIManager.cs
+++ Assets/_FlowSystem/GameplayUI/ManaUIManager.cs
@@ -8,18 +8,20 @@ using UnityEngine.UI;
 
 public class ManaUIManager : MonoBehaviour
 {
-    [SerializeField] private float animationProgressDuration = 0.5f;
+    [SerializeField] private float defaultAnimationProgressDuration = 0.5f;
 
     [SerializeField] private List<Slider> manaProgressBars;
 
 
     [Button]
-    public void UpdateManaBar(int index, float value, Action onComplete = null)
+    public void UpdateManaBar(int index, float value, float duration, Action onComplete = null)
     {
         DOTween.Kill(manaProgressBars[index]);
-        manaProgressBars[index].DOValue(value, animationProgressDuration).OnComplete(() =>
+        manaProgressBars[index].DOValue(value, duration > 0 ? duration : defaultAnimationProgressDuration).OnComplete(() =>
         {
             onComplete?.Invoke();
         });
     }
+
+
 }
